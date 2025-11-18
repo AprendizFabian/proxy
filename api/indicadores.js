78c3d8d680a0022277ff71f88a687caf7ade61b8
fecristancho <fabian2006cristancho@gmail.com>
2025-11-18 11:50:22 -0500

    agrago api para indicadores

---
+++ api/indicadores.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") return res.status(200).end();
+
+  const url = "http://redash.rappi.com/api/queries/124731/results.json?api_key=dQhB16NHyUK5L22Uv5OUtsLXmzisGhFFiqocwQLw";
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Error Redash:", error);
+    res.status(500).json({ error: "Error al conectar con Redash (Top Mejores)" });
+  }
+}
